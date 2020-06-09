ca322f4e37f9d5969398196928d9444d5b6a97a8
xdays <easedays@gmail.com>
2020-06-09 09:27:39 +0000

    udpate entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@
 set -e
 
 ts=$(date +%s)
-echo "$INPUT_KUBECONFIG" | base64 --decode > /tmp/config-$ts
+echo "$INPUT_KUBECONFIG" | base64 -d > /tmp/config-$ts
 export KUBECONFIG=/tmp/config-$ts
 
 sh -c "kubectl $*"
